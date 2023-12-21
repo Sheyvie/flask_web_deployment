ae21f45d6c530eaa2d058bc9744e1d410a588568
Sheyvie <sheilayvi@gmail.com>
2023-12-21 14:29:00 +0300

    Web deployment

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+"""Summation"""
+
+def summation(a, b):
+    """Addition calculation"""
+    return a + b
+def subtraction(a, b):
+    """Subtraction calculation"""
+    return a - b
+def multiplication(a, b):
+    """Subtraction calculation"""
+    return a * b
